a72e523c777c9de3d5000cc4ef78ec03bb9c6896
Bogdan <paraschiv_bogdan_george@yahoo.com>
2018-11-05 20:24:09 +0200

    First connection

--- HC05.cpp
+++ HC05.cpp
@@ -1,7 +1,16 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+
+#define RX 2
+#define TX 3
+
+
+SoftwareSerial HC05(RX, TX);		// RX, TX
 
 void setup(void){
 	Serial.begin(115200);
+
+	HC05.begin(9600);
 }
 
 void loop(void){
