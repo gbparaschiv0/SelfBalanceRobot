89e15a5749334fa13f1fdeae249ef98d0db6360a
Bogdan <paraschiv_bogdan_george@yahoo.com>
2018-11-05 20:37:11 +0200

    communication working. GG Daniel

--- HC05.cpp
+++ HC05.cpp
@@ -4,16 +4,16 @@
 #define RX 2
 #define TX 3
 
-
 SoftwareSerial HC05(RX, TX);		// RX, TX
 
-void setup(void){
+void setup(void) {
 	Serial.begin(115200);
 
 	HC05.begin(9600);
 }
 
-void loop(void){
-	Serial.println("Hello world!!!");
-	delay(2000);
+void loop(void) {
+	if (HC05.available()) {
+		Serial.write(HC05.read());
+	}
 }
