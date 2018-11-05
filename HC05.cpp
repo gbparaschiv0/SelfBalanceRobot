f73cb1a2ea6eac996fb668d8773e5c682df811c0
Bogdan <paraschiv_bogdan_george@yahoo.com>
2018-11-06 00:44:47 +0200

    AT mode for HC05

--- HC05.cpp
+++ HC05.cpp
@@ -6,14 +6,23 @@
 
 SoftwareSerial HC05(RX, TX);		// RX, TX
 
-void setup(void) {
-	Serial.begin(115200);
-
-	HC05.begin(9600);
+void setup()
+{
+  pinMode(12, OUTPUT);  // this pin will pull the HC-05 pin 34 (key pin) HIGH to switch module to AT mode
+  digitalWrite(12, HIGH);
+  Serial.begin(9600);
+  Serial.println("Enter AT commands:");
+  HC05.begin(38400);  // HC-05 default speed in AT command more
 }
 
-void loop(void) {
-	if (HC05.available()) {
-		Serial.write(HC05.read());
-	}
+void loop()
+{
+
+  // Keep reading from HC-05 and send to Arduino Serial Monitor
+  if (HC05.available())
+    Serial.write(HC05.read());
+
+  // Keep reading from Arduino Serial Monitor and send to HC-05
+  if (Serial.available())
+	  HC05.write(Serial.read());
 }
