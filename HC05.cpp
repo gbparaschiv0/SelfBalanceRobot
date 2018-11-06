706c5d147729ced01ebc53e75c049f91b2fca049
Bogdan <paraschiv_bogdan_george@yahoo.com>
2018-11-06 19:12:50 +0200

    First conversion implementation

--- HC05.cpp
+++ HC05.cpp
@@ -9,7 +9,12 @@
 SoftwareSerial HC05(RX, TX);		// RX, TX
 uint32_t loopTimer;
 bool bOverTimeAlertLed = false;		// Semaphore in case the loop is taking longer to finish
-char readChar;
+
+bool bStart;
+char value;
+char buffLen;
+unsigned short dataXY[2];		// x = 0, y = 1
+unsigned short myX;
 
 void setup(void) {
 	pinMode(LED, OUTPUT);
@@ -21,12 +26,36 @@ void setup(void) {
 	loopTimer = micros() + 4000;
 }
 
+
+
 void loop(void) {
 
-	while (HC05.available()) {
-		readChar = (char)HC05.read();
-		Serial.print(readChar);
+	if (HC05.available()) {
+
+		value = (char) HC05.read();
+
+		if (value == 2) {
+			bStart = true;
+			buffLen = 0;
+			dataXY[0] = 0;
+			dataXY[1] = 0;
+		} else if (value == 3) {
+			bStart = false;
+			if (buffLen <= 6) {
+				myX = dataXY[0];
+			}
+			// Processing data
+		} else if (bStart) {
+			if (buffLen < 6) {
+				dataXY[buffLen / 3] *= 10;
+				dataXY[buffLen / 3] += value - 48;
+			}
+
+			buffLen++;
+		}
 	}
+
+	Serial.println(myX);
 
 	if (bOverTimeAlertLed)
 		digitalWrite(LED, HIGH);
