19408ad81ad4be26d9a0c17af4cc8a55bb4f7f56
Bogdan <paraschiv_bogdan_george@yahoo.com>
2018-11-07 22:27:15 +0200

    Reading is working and bug fixed

--- HC05.cpp
+++ HC05.cpp
@@ -6,15 +6,22 @@
 // Led pin define
 #define LED 13
 
+#define NR_TO_CH(x) (x + 48)		// Convert number to ASCII
+#define CH_TO_NR(x) (x - 48)		// Convert ASCII to nr
+
 SoftwareSerial HC05(RX, TX);		// RX, TX
 uint32_t loopTimer;
-bool bOverTimeAlertLed = false;		// Semaphore in case the loop is taking longer to finish
+bool bOverTimeAlertLed = false;	// Semaphore in case the loop is taking longer to finish
 
 bool bStart;
 char value;
 char buffLen;
 unsigned short dataXY[2];		// x = 0, y = 1
 unsigned short myX;
+
+char buttonCommand;
+char temp[20];
+char lastReadCounter;
 
 void setup(void) {
 	pinMode(LED, OUTPUT);
@@ -26,14 +33,18 @@ void setup(void) {
 	loopTimer = micros() + 4000;
 }
 
-
-
 void loop(void) {
 
 	if (HC05.available()) {
 
 		value = (char) HC05.read();
+		if (lastReadCounter > 19) {
+			lastReadCounter = 0;
+			memset(temp, 0x00, sizeof(temp));
+		}
 
+		temp[lastReadCounter] = value;
+		lastReadCounter++;
 		if (value == 2) {
 			bStart = true;
 			buffLen = 0;
@@ -41,21 +52,46 @@ void loop(void) {
 			dataXY[1] = 0;
 		} else if (value == 3) {
 			bStart = false;
-			if (buffLen <= 6) {
+			if (buffLen == 6) {
 				myX = dataXY[0];
+				if (myX < 100 || myX > 300) {
+					Serial.println("Wrong value received:");
+					for (int i = 0; i < 20; i++) {
+						Serial.print((short) temp[i]);
+						Serial.print("\t");
+					}
+					Serial.println();
+					Serial.println("Converted:");
+					for (int i = 0; i < 20; i++) {
+						Serial.print((short) (temp[i] - 48));
+						Serial.print("\t");
+					}
+					Serial.println();
+					Serial.print("BuffLen: ");
+					Serial.print((short) buffLen);
+					while (1)
+						;
+				}
 			}
-			// Processing data
-		} else if (bStart) {
+			else if(buffLen == 1 && NR_TO_CH(dataXY[0]) > 64){
+				buttonCommand = NR_TO_CH(dataXY[0]);
+			}
+		}
+		// Processing data
+		else if (bStart) {
 			if (buffLen < 6) {
 				dataXY[buffLen / 3] *= 10;
-				dataXY[buffLen / 3] += value - 48;
+				dataXY[buffLen / 3] += CH_TO_NR(value);
 			}
 
 			buffLen++;
 		}
 	}
-
-	Serial.println(myX);
+	//Serial.println(myX);
+	if(buttonCommand){
+		Serial.println(buttonCommand);
+		buttonCommand = 0;
+	}
 
 	if (bOverTimeAlertLed)
 		digitalWrite(LED, HIGH);
